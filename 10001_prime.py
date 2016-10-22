f7f45126e89eba05b98c38d4012f4f13bb2d7151
root <pradyuachyuthuni@gmail.com>
2016-10-22 23:15:37 +0530

    Euler_Project_Problem_7

---
+++ 10001_prime.py
@@ -0,0 +1,20 @@
+import is_prime as ip
+import sys
+
+def main():
+	prime_count = 1 # took care of 2.
+	number = 3
+
+	while prime_count != 10001:
+		if ip.is_prime(number):
+			prime_count += 1
+		number += 2 
+	
+	if prime_count == 1:
+		print(number -1)
+		sys.exit()
+
+	print(number - 2)
+
+if __name__ == '__main__':
+	main()
