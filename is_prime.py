ff4c94f8e14dd3f40ccfa4dd940e704f8d06c478
root <pradyuachyuthuni@gmail.com>
2016-10-22 18:24:21 +0530

    Program to check whether a number is prime or not.

---
+++ is_prime.py
@@ -0,0 +1,33 @@
+import sys
+
+def main():
+	args = sys.argv
+	
+	if len(args) == 1:
+		print('Argument not passed. Please check again')
+		sys.exit()
+	number = int(args[1])
+
+	if number == 2:
+		print("Number is the only even prime.")
+	elif is_even(number):
+		print("Number is not prime, its composite.")
+	else:
+		for i in range(3,number/2,2):
+			if number % i == 0:
+				break
+		print("Number is prime.")
+
+def is_even(num):
+	if num % 2 == 0:
+		return True
+	else:
+		return False
+
+if __name__ == '__main__':
+	main()	
+		
+ 
+
+
+
